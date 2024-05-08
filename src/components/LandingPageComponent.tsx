0b615302b7ec9f3f64c805e88728db40913c0643
JayvonK <jaykhuth@gmail.com>
2024-05-08 13:43:05 -0700

    hover taken out

--- src/components/LandingPageComponent.tsx
+++ src/components/LandingPageComponent.tsx
@@ -19,12 +19,12 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
         <div className="grid xl:grid-cols-[75%_25%] mb-4">
             <div className="grid grid-flow-row xl:mr-4">
 
-                <div className="rounded-lg bgCard border-[0.8px] border-zinc-600 hover:border-blue-500 mb-4 sm:hidden flex justify-center ">
+                <div className="rounded-lg bgCard border-[0.8px] border-zinc-600  mb-4 sm:hidden flex justify-center ">
                     <img className="w-[80%] h-40 object-cover object-top" src="/images/Jayvon1.png" alt="" />
                 </div>
 
                 {/* Row 1 */}
-                <div className="min-h-56 rounded-lg bgCard p-6 border-[0.8px] border-zinc-600 hover:border-blue-500 mb-4 ">
+                <div className="min-h-56 rounded-lg bgCard p-6 border-[0.8px] border-zinc-600  mb-4 ">
                     <div className="flex justify-between w-full">
                         <div className="flex flex-col justify-between lg:max-w-[60%] sm:max-w-[55%]">
                             <div>
@@ -34,19 +34,19 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
 
                             <div id="button links">
                                 <a href="https://www.linkedin.com/in/jayvon-khuth-1363ab2a0/" target="_blank">
-                                    <div className="py-3 px-6 border-[0.5px] bg-white hover:border-blue-500 shadow-lg hover:shadow-blue-500 inline-block rounded-lg mr-5">
+                                    <div className="py-3 px-6 border-[0.5px] bg-white  shadow-lg hover:shadow-blue-500 inline-block rounded-lg mr-5">
                                         <img className="w-6 aspect-square object-cover" src="/images/linkedin.svg" alt="" />
                                     </div>
                                 </a>
 
                                 <a href="https://github.com/JayvonK" target="_blank">
-                                    <div className="py-3 px-6 bg-white border-[0.5px] hover:border-blue-500 shadow-lg hover:shadow-blue-500 inline-block rounded-lg mr-5">
+                                    <div className="py-3 px-6 bg-white border-[0.5px]  shadow-lg hover:shadow-blue-500 inline-block rounded-lg mr-5">
                                         <img className="w-6 aspect-square object-cover" src="/images/github.svg" alt="" />
                                     </div>
                                 </a>
 
                                 <a href="https://docs.google.com/document/d/1DrDMeP0BlD9Q6uYFdgPx4-UIAeSm6IovQLcJcCh6nrc/edit?usp=sharing" target="_blank">
-                                    <div className="py-3 px-6 border-[0.5px] bg-white hover:border-blue-500 shadow-lg hover:shadow-blue-500 inline-block rounded-lg">
+                                    <div className="py-3 px-6 border-[0.5px] bg-white  shadow-lg hover:shadow-blue-500 inline-block rounded-lg">
                                         <img className="w-6 aspect-square object-cover" src="/images/googledocs.svg" alt="" />
                                     </div>
                                 </a>
@@ -61,7 +61,7 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
 
                 {/* About me hidden */}
 
-                <div className="rounded-lg bgCard border-[0.8px] border-zinc-600 hover:border-blue-500 p-6 mb-4 xl:hidden block">
+                <div className="rounded-lg bgCard border-[0.8px] border-zinc-600  p-6 mb-4 xl:hidden block">
                     <h3 className="font-bold mb-3 lg:text-2xl text-xl text-white">About Me</h3>
                     <ul className="text-gray-300 lg:text-base text-sm">
                         <li className="mb-3">I&apos;m a 18 year old web developer from Stockton, CA</li>
@@ -79,7 +79,7 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
 
                 {/* Row 2 */}
                 <div className="w-full md:flex">
-                    <div className="md:w-[40%] rounded-lg bgCard border-[0.8px] border-zinc-600 hover:border-blue-500 p-6 md:mr-4 md:mb-0 mb-4">
+                    <div className="md:w-[40%] rounded-lg bgCard border-[0.8px] border-zinc-600  p-6 md:mr-4 md:mb-0 mb-4">
                         <h3 className="font-bold mb-1 lg:text-2xl text-xl text-white">Contact Me</h3>
                         <p className="text-gray-300 mb-3 lg:text-base text-sm">Feel free contact me through email/number or through socials</p>
                         <div className="md:block flex justify-between">
@@ -114,14 +114,14 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
                         </div>
                     </div>
 
-                    <div className="rounded-lg bgCard border-[0.8px] w-full border-zinc-600 hover:border-blue-500 p-6">
+                    <div className="rounded-lg bgCard border-[0.8px] w-full border-zinc-600  p-6">
                         <h3 className="font-bold mb-3 lg:text-2xl text-xl text-white">Current Project</h3>
                         <p className="text-gray-400 mb-4 lg:text-base text-sm">What Am I Currently Working On?</p>
                         <div className="grid sm:grid-cols-2 gap-4">
                             <div>
                                 <div className="hover:cursor-pointer">
                                     <a href='https://full-stack-strike-showdown.vercel.app/' target='_blank'>
-                                        <img src="/images/StrikeShowdown2.png" className="h-52 object-cover showdownBox border border-gray-500" alt="" />
+                                        <img src="/images/StrikeShowdown2.png" className="h-52 object-cover showdownBox border border-gray-500 hover:border-orange-300" alt="" />
                                     </a>
                                 </div>
                                 <div className="flex justify-between mt-10">
@@ -135,7 +135,7 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
                             <div className="sm:pl-4 sm:mt-0 mt-4">
                                 <h3 className="text-white lg:text-2xl text-xl font-bold mb-1"><span className="text-orange-400">Strike</span> Showdown</h3>
                                 <p className="text-gray-300 mb-4 lg:text-base text-sm">Currently I am a team lead, guiding 2 other web developers to create a fullstack application aimed at the audience of bowlers, called &quot;Strike Showdown&quot;</p>
-                                <button className="text-black font-semibold rounded-lg bg-white p-2 shadow-lg border hover:shadow-blue-500 hover:border-blue-500" onClick={props.openProjects}>View All Projects</button>
+                                <button className="text-black font-semibold rounded-lg bg-white p-2 shadow-lg border hover:shadow-blue-500 " onClick={props.openProjects}>View All Projects</button>
                             </div>
 
                         </div>
@@ -147,7 +147,7 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
 
 
             <div className="text-gray-300">
-                <div className="rounded-lg bgCard border-[0.8px] border-zinc-600 hover:border-blue-500 p-6 mb-4 xl:block hidden">
+                <div className="rounded-lg bgCard border-[0.8px] border-zinc-600  p-6 mb-4 xl:block hidden">
                     <h3 className="font-bold mb-3 lg:text-2xl text-xl text-white">About Me</h3>
                     <ul>
                         <li className="mb-3">I&apos;m a 18 year old web developer from Stockton, CA</li>
@@ -164,7 +164,7 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
                 </div>
 
                 <div className="grid xl:grid-cols-none grid-cols-2 xl:gap-0 gap-4 xl:mt-0 mt-4">
-                    <div className="rounded-lg bgCard border-[0.8px] border-zinc-600 hover:border-blue-500 p-6 xl:mb-4">
+                    <div className="rounded-lg bgCard border-[0.8px] border-zinc-600  p-6 xl:mb-4">
                         <h3 className="text-xl text-white font-bold mb-4">Languages:</h3>
 
                         <div>
@@ -187,7 +187,7 @@ const LandingPageComponent = (props: { openProjects: () => void }) => {
 
                     </div>
 
-                    <div className="rounded-lg bgCard border-[0.8px] border-zinc-600 hover:border-blue-500 p-6">
+                    <div className="rounded-lg bgCard border-[0.8px] border-zinc-600  p-6">
                         <h3 className="text-xl text-white font-bold mb-4">Frameworks:</h3>
 
                         <div>
