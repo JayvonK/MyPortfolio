a4bbfae8186a82045418f239c19f324fc853e61a
JayvonK <jaykhuth@gmail.com>
2024-05-13 06:41:44 -0700

    lang page card changes

--- src/components/LangCardComponent.tsx
+++ src/components/LangCardComponent.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const LangCardComponent = (props: { color: string, level: string, img: string, name: string, fav: boolean }) => {
+const LangCardComponent = (props: { level: string, img: string, name: string, fav: boolean }) => {
     return (
-        <div className={`p-6 bgCard border relative border-${props.color} rounded-md`}>
+        <div className={props.level === "Strong" ? "p-6 bgCard border relative border-white rounded-md" : "p-6 bgCard border relative border-zinc-500 rounded-md"}>
             {props.fav && <img className='absolute top-2 right-2 sm:w-7 w-3' src="/images/heart-fill.svg" alt="" />}
             <div className='flex items-center h-full'>
                 <img className='sm:w-12 w-6 aspect-square mr-4' src={props.img} alt="" />
-                <h2 className='text-white sm:text-xl text-base w-full text-center'>{props.name}</h2>
+                <h2 className={props.level === "Strong" ? "text-white sm:text-xl text-base w-full text-center font-semibold" : "text-white sm:text-xl text-base w-full text-center"}>{props.name}</h2>
             </div>
         </div>
     )
